2e2ecccee551df627f8f8bd32f321f3562481f10
Ivan Stoyanov <86497265+Vanko888@users.noreply.github.com>
2022-03-14 18:03:29 +0200

    Add project files.

---
+++ Shapes/Shape.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Channels;
+
+namespace Shapes
+{
+    public abstract class Shape
+    {
+        public abstract double CalculatePerimeter();
+
+        public abstract double CalculateArea();
+
+        public virtual string Draw()
+        {
+            return "Drawing ";
+        }
+    }
+}
